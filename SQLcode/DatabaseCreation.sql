0e542e5b51d445743e5c6badcf838b2d63bd7640
zhouzhou <32453952+zhouzhou514@users.noreply.github.com>
2021-05-18 21:11:02 +0800

    Update DatabaseCreation.sql

--- SQLcode/DatabaseCreation.sql
+++ SQLcode/DatabaseCreation.sql
@@ -1,8 +1,63 @@
 -- run this script to create the SWS database.
+-- remember to drop each table before creating it!!!
+-- see relational data model diagram for more information.
 show full tables;
 select * from testsample;
+
+
 -- create customer table
+drop table Customer;
 create table Customer(
 customerId int,
 primary key (customerId)
+);
+
+
+-- create booking table
+drop table ;
+create table (
+,
+primary key ()
+);
+
+-- create addonserivces table
+drop table ;
+create table (
+,
+primary key ()
+);
+
+-- create site table
+drop table ;
+create table (
+,
+primary key ()
+);
+
+-- create staff table
+drop table ;
+create table (
+,
+primary key ()
+);
+
+-- create instructor table
+drop table ;
+create table (
+,
+primary key ()
+);
+
+-- create equipment table
+drop table ;
+create table (
+,
+primary key ()
+);
+
+-- create lesson table
+drop table ;
+create table (
+,
+primary key ()
 );
