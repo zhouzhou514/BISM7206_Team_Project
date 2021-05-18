5361e0bf310d8b23c15e4238f7d5098573e8dafc
zhouzhou <32453952+zhouzhou514@users.noreply.github.com>
2021-05-18 18:53:07 +0800

    Update DatabaseCreation.sql

--- SQLcode/DatabaseCreation.sql
+++ SQLcode/DatabaseCreation.sql
@@ -1,3 +1,8 @@
 -- run this script to create the SWS database.
 show full tables;
 select * from testsample;
+-- create customer table
+create table Customer(
+customerId int,
+primary key (customerId)
+);
