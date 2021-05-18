fe44e3019f5039f00b4683967c96e83ed3cbb904
zhouzhou <32453952+zhouzhou514@users.noreply.github.com>
2021-05-18 18:12:16 +0800

    Add files via upload

---
+++ DatabaseCreation.sql
@@ -0,0 +1,3 @@
+-- run this script to create the SWS database.
+show full tables;
+select * from testsample;
---
+++ testScript.sql
@@ -0,0 +1,9 @@
+create table testsample
+(
+testValueOne int,
+testCharOne varchar(255),
+testCharTwo char
+);
+
+
+select * from testsample;
