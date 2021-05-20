8c8766f867b4c5398797e1468b48f1cb272460fb
zhouzhou <32453952+zhouzhou514@users.noreply.github.com>
2021-05-21 00:41:13 +0800

    Update DatabaseQueries.sql

--- SQLcode/DatabaseQueries.sql
+++ SQLcode/DatabaseQueries.sql
@@ -2,3 +2,12 @@
 
 show full tables;
 drop table testsample;
+
+
+
+-- Which lesson or service is mostly welcomed by the customers?
+select bk.serviceId,count(*),sv.serviceName from booking bk 
+inner join Service sv on sv.serviceId=bk.serviceId
+group by bk.serviceId
+order by count(*);
+-- this script above shows the most frequently selected lession or plan selected by customer
