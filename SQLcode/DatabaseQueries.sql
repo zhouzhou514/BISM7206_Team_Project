c9d42c243dc8f2c0b390362610ed78a8bfb1dc95
zhouzhou <32453952+zhouzhou514@users.noreply.github.com>
2021-05-21 01:20:10 +0800

    Update DatabaseQueries.sql

--- SQLcode/DatabaseQueries.sql
+++ SQLcode/DatabaseQueries.sql
@@ -10,4 +10,10 @@ select bk.serviceId,count(*),sv.serviceName from booking bk
 inner join Service sv on sv.serviceId=bk.serviceId
 group by bk.serviceId
 order by count(*);
--- this script above shows the most frequently selected lession or plan selected by customer
+-- this script above shows the most frequently selected lession or plan by customer
+
+-- What's the gender ratio of customers? This will help SWS marketing better.
+select cs.gender,count(*) * 100.0 / sum(count(*)) over() from booking bk 
+inner join customer cs on cs.customerId=bk.customerId
+group by cs.gender;
+-- this script genrate the gender ratio of customers.
