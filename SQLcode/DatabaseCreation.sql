6cabd3e60f5749471cc3084a422366b85e7c6ddf
zhouzhou <32453952+zhouzhou514@users.noreply.github.com>
2021-05-20 20:10:16 +0800

    Update DatabaseCreation.sql

--- SQLcode/DatabaseCreation.sql
+++ SQLcode/DatabaseCreation.sql
@@ -1,6 +1,8 @@
 -- run this script to create the SWS database.
 -- remember to drop each table before creating it!!!
 -- see relational data model diagram for more information.
+drop table if exists plan,lesson,basicservice;
+
 show full tables;
 
 
@@ -11,22 +13,22 @@ create table Customer(
   fName varchar(50) not null,
   lName varchar(50),
   dateOfBirth varchar(20),
-  Address varchar(50),
-  Phone varchar(10) not null,
+  address varchar(50),
+  phone varchar(10) not null,
   primary key (customerId)
 );
 
 -- create Staff table
 drop table if exists Staff;
 create table Staff(
-  StaffId char(10) not null unique,
+  staffId char(10) not null unique,
   fName varchar(50) not null,
   lName varchar(50),
   position varchar(20), -- job title
-  City varchar(50),
-  Street varchar(50),
-  Postcode varchar(6),
-  Phone varchar(10) not null,
+  city varchar(50),
+  street varchar(50),
+  postcode varchar(6),
+  phone varchar(10) not null,
   isInstructor bit not null,
   primary key (staffId)
 );
@@ -46,11 +48,11 @@ create table Instructor(
 --  equipment hiring and any kinds of lesson are considered as service
 drop table if exists Service;
 create table Service(
-  ServiceId char(10) not null unique,
-  ServiceName varchar(50) not null,
+  serviceId char(10) not null unique,
+  serviceName varchar(50) not null,
   duration int,
-  ServiceFee int not null,
-  ServiceDescription varchar(255),
+  serviceFee int not null,
+  serviceDescription varchar(255),
   primary key (ServiceId)
 );
 
@@ -81,14 +83,14 @@ create table Booking(
   bookingId char(10) not null unique,
   customerId char(10) not null, 
   instructorId char(10),
-  ServiceId char(10) not null,
+  serviceId char(10) not null,
   siteId char(10) not null,
   equipmentId char(10),
   startTime datetime,
   primary key (bookingId),
-  foreign key (customerId) references customer(customerId),
-  foreign key (instructorId) references Instructor(instructorId),
-  foreign key (ServiceId) references Service(ServiceId),
-  foreign key (siteId) references Site(siteId),
-  foreign key (equipmentId) references Equipment(equipmentId)
+  foreign key (customerId) references customer(customerId) on delete cascade on update cascade,
+  foreign key (instructorId) references Instructor(instructorId) on delete cascade on update cascade,
+  foreign key (ServiceId) references Service(ServiceId) on delete cascade on update cascade,
+  foreign key (siteId) references Site(siteId) on delete cascade on update cascade,
+  foreign key (equipmentId) references Equipment(equipmentId) on delete cascade on update cascade
 );
